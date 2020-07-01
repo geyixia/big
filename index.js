92d117e6665ce492e7c0cb0e16f2a6d40c05de23
geyixia <2213106680@qq.com>
2020-07-01 08:48:02 +0800

    test

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const cors = require('cors')
+const path = require('path')
+const loginRouter = require(path.join(__dirname, 'routers/login.js'))
+const app = express()
+
+// 处理客户端请求post参数
+// for parsing application/json
+app.use(express.json()) 
+// for parsing application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true })) 
+
+// 设置跨域
+app.use(cors())
+
+// 设置路径
+app.use('/api', loginRouter)
+
+app.listen(8888, () => {
+  console.log('running...')
+})
---
+++ routers/login.js
@@ -0,0 +1,26 @@
+/*
+  统一管理路由信息
+*/
+const express = require('express')
+const path = require('path')
+const db = require(path.join(__dirname, '../common/db.js'))
+const router = express.Router()
+
+router.post('/login', (req, res) => {
+  res.send('login')
+})
+
+router.post('/reguser', (req, res) => {
+  res.send('reguser')
+})
+
+router.get('/test', async (req, res) => {
+  let sql = 'select * from users'
+  let ret = await db.operateData(sql, null)
+  res.json({
+    status: 0,
+    data: ret
+  })
+})
+
+module.exports = router
